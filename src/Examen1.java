9a8ffce8c72839649c744ed09f751f2d6b72484c
oscar <oscar@192.168.0.13>
2018-03-23 13:22:43 -0600

    version 2

--- src/Examen1.java
+++ src/Examen1.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.border.TitledBorder;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -13,23 +16,30 @@ import java.awt.event.ActionListener;
 
 public class Examen1 extends JFrame implements ActionListener{
 	
-	private JPanel  panelInf, panelCen;
-	private JButton btnCalcular;
+	private JPanel  panel, panelInf, panelCen;
+	private JButton btnCalcular,btnChange;
 	private JLabel labV, labD, labT;
 	private JTextField jtfV, jtfD, jtfT;
 	
 	public Examen1(String titulo) {
-		setSize(350,130);
+		setSize(350,150);
 		setLayout(new BorderLayout());
+		setTitle(titulo);
 		
 		Container contentPane = getContentPane();
 		
+		panel = new JPanel();
 		panelInf = new JPanel();
 		panelCen = new JPanel();
 		panelCen.setLayout(new GridLayout(0,3));
 		
-		contentPane.add(panelCen, BorderLayout.NORTH);
-		contentPane.add(panelInf, BorderLayout.CENTER);
+		TitledBorder border = BorderFactory.createTitledBorder("Examen");
+		border.setTitleColor(Color.BLACK);
+		panel.setBorder(border);
+		
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.add(panelCen, BorderLayout.NORTH);
+		panel.add(panelInf, BorderLayout.CENTER);
 		
 		addComponent();
 		
@@ -47,6 +57,8 @@ public class Examen1 extends JFrame implements ActionListener{
 		//boton
 		btnCalcular = new JButton("Calcular");
 		btnCalcular.addActionListener(this);
+		btnChange = new JButton("Cambiar calculo");
+		btnChange.addActionListener(this);
 		
 		panelCen.add(labV);
 		panelCen.add(labD);
@@ -57,6 +69,7 @@ public class Examen1 extends JFrame implements ActionListener{
 		panelCen.add(jtfT);
 		
 		panelInf.add(btnCalcular);
+		panelInf.add(btnChange);
 		
 	}
 	
@@ -72,6 +85,8 @@ public class Examen1 extends JFrame implements ActionListener{
 			}else{
 				JOptionPane.showMessageDialog(null, "Ingrese datos", "", JOptionPane.INFORMATION_MESSAGE);
 			}
+		}else if(ev.getSource() == btnChange) {
+			Opciones();
 		}
 	}
 	
@@ -95,9 +110,45 @@ public class Examen1 extends JFrame implements ActionListener{
 		int velocidad = distancia / tiempo;
 		JOptionPane.showMessageDialog(null, "Velocidad: "+velocidad+" m/s", "Resultado", JOptionPane.INFORMATION_MESSAGE);
 	}
+	
+	public void Opciones() {
+		String[] opciones = {"Velocidad","Distancia","Tiempo","Salir"};
+	    int opcion = JOptionPane.showOptionDialog(
+	        null
+	       ,"Seleccione una opcion a calcular"
+	       ,"Ventana de opciones"
+	       ,JOptionPane.DEFAULT_OPTION
+	       ,JOptionPane.QUESTION_MESSAGE
+	       ,null
+	       ,opciones
+	       ,null
+	    );
+
+	    switch(opciones[opcion]){
+	      case "Velocidad":
+	    	  jtfV.setEditable(false);
+	    	  jtfT.setEditable(true);
+	    	  jtfD.setEditable(true);
+	        break;
+	      case "Distancia":
+	    	  jtfD.setEditable(false);
+	    	  jtfV.setEditable(true);
+	    	  jtfT.setEditable(true);
+	        break;
+	      case "Tiempo":
+	    	  jtfT.setEditable(false);
+	    	  jtfD.setEditable(true);
+	    	  jtfV.setEditable(true);
+	        break;
+	      case "Salir":
+	    	  System.exit(0);
+	        break;
+	    }
+	}
 
 	public static void main(String[] args) {
-		JFrame frame = new Examen1("Examen");
+		Examen1 frame = new Examen1("Examen");
+		frame.Opciones();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
